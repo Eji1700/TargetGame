50af88754e60f6a8be37c8b7ba8864c911c57cc6
Eji1700 <regulus87@yahoo.com>
2022-01-04 22:06:37 -0800

    target now jumps randomly

--- Game1.fs
+++ Game1.fs
@@ -4,6 +4,7 @@ open Microsoft.Xna.Framework
 open Microsoft.Xna.Framework.Graphics
 open Microsoft.Xna.Framework.Content
 open Microsoft.Xna.Framework.Input 
+open System
 
 type TargetGame () as x =
     inherit Game()
@@ -28,6 +29,7 @@ type TargetGame () as x =
     let quitGame (game: TargetGame) =
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back = ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape)) then 
             game.Exit()
+
     // let (|KeyDown|_|) k (state: KeyboardState) =
     //     if state.IsKeyDown k then Some() else None
 
@@ -52,7 +54,11 @@ type TargetGame () as x =
         let mouseTargetDist =  Vector2.Distance(targetPosition, mState.Position.ToVector2())
         match mState.LeftButton, mRelease with 
         | ButtonState.Pressed, true -> 
-            if mouseTargetDist <= targetRadius then Score <- Score + 1
+            if mouseTargetDist <= targetRadius then 
+                Score <- Score + 1
+                let rand = Random()
+                targetPosition.X <- rand.Next(0, graphics.PreferredBackBufferWidth) |> float32
+                targetPosition.Y <- rand.Next(0, graphics.PreferredBackBufferHeight) |> float32
             mRelease <- false
         | ButtonState.Released, false -> 
             mRelease <- true
